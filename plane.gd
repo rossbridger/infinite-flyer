f47a69390a56c9a59dc14a000cf6691938ede7da
Ross Charles C. <10781734+rossbridger@users.noreply.github.com>
2026-03-15 03:22:01 +0800

    Update
    Signed-off-by: Ross Charles C. <10781734+rossbridger@users.noreply.github.com>

--- plane.gd
+++ plane.gd
@@ -1,13 +1,24 @@
 extends CharacterBody3D
 
-@export var pitch_speed = 0
-@export var roll_speed = 0
-@export var level_speed = 0
+@export var pitch_speed = 1.1
+@export var roll_speed = 2.5
+@export var level_speed = 4.0
 @export var forward_speed = 25
 
 var pitch_input = 0
 var roll_input = 0
 var max_altitude = 20
+
+signal dead
+signal score_changed(score)
+signal fuel_changed(fuel)
+
+@export var fuel_burn = 1.0
+var max_fuel = 10.0
+var fuel = 10.0:
+	set = set_fuel
+var score = 0:
+	set = set_score
 
 func get_input(delta):
 	pitch_input = Input.get_axis("pitch_down", "pitch_up")
@@ -25,3 +36,26 @@ func _physics_process(delta: float) -> void:
 	velocity = -transform.basis.z * forward_speed
 	velocity += transform.basis.x * $cartoon_plane.rotation.z / deg_to_rad(45) * forward_speed / 2.0
 	move_and_slide()
+	fuel -= fuel_burn * delta
+	if get_slide_collision_count() > 0:
+		die()
+
+func die():
+	set_physics_process(false)
+	$AudioStreamPlayer3D.play()
+	$cartoon_plane.hide()
+	$Explosion.show()
+	$Explosion.play("default")
+	await $Explosion.animation_finished
+	$Explosion.hide()
+	dead.emit()
+
+func set_fuel(value):
+	fuel = min(value, max_fuel)
+	fuel_changed.emit(fuel)
+	if fuel <= 0:
+		die()
+
+func set_score(value):
+	score = value
+	score_changed.emit(score)
